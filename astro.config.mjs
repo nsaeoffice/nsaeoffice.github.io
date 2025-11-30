02256dab027bcba4328710c8035e751a18794a66
nsae <nsaeoffice@gmail.com>
2025-11-30 12:17:40 +0545

    fix:site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://nsaeoffice.github.io",
+  site: "https://nsae-nepal.github.io",
   integrations: [mdx(), sitemap(), react()],
   vite: {
     plugins: [tailwindcss()],
